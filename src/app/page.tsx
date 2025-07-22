b6927d472684d5aca364b1e251f5441bfc812c20
judhisahoo <judhisahoo@gmail.com>
2025-07-22 20:10:53 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+const poppp = 'test'
+export default function Home() {
+  const name = 'test'
+  return (
+    <div className={styles.page}>
+      <main> Home</main>
+    </div>
+  )
+}
