fb9bcacf9f2a37ef6f3ad1fd976c4b85f8e8e7e7
judhisahoo <judhisahoo@gmail.com>
2025-07-22 20:53:08 +0530

    update lint command to package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
-const poppp = 'test'
 export default function Home() {
-  const name = 'test'
   return (
     <div className={styles.page}>
       <main> Home</main>
